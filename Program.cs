84b8e8b2b4e912595b443081cd0a6a804027171d
Tim Suvorov <timsuvorov@icloud.com>
2024-10-09 21:23:31 +0200

    fixed design :sparkles:

--- Program.cs
+++ Program.cs
@@ -2,20 +2,21 @@
 
 //Timofey Suvorov, NET 2024
 
+Console.WriteLine("Prensetation of a dog:\n");
 Dog dog1 = new Dog("dog", 3, 35, "dry food and a lot of meat", "home", "Pitbull");
 dog1.DisplayInfo();
 dog1.MakeSound();
 dog1.Hunt();
 dog1.DoTrick();
 
-Console.WriteLine("\n");
+Console.WriteLine("\nPresentation of a lion:\n");
 Lion lion1 = new Lion("lion", 9, 175, "meat", "in the Safari", "the King of the Jungle");
 lion1.DisplayInfo();
 lion1.MakeSound();
 lion1.Hunt();
 lion1.TheKing();
 
-Console.WriteLine("\n");
+Console.WriteLine("\nPresentation of a shark:\n");
 
 Shark shark1 = new Shark("shark", 20, 800, "fishes", "Ocean", true);
 shark1.DisplayInfo();
@@ -23,7 +24,7 @@ shark1.MakeSound();
 shark1.Hunt();
 Console.WriteLine(shark1.CantBreath());
 
-Console.WriteLine("\n");
+Console.WriteLine("\nPresentation of a cute dog:\n");
 
 ShihTzu sh = new ShihTzu("dog", 3, 6, "dry food", "home", "Shih-Tzu", "small");
 sh.DisplayInfo();
@@ -31,7 +32,7 @@ sh.MakeSound();
 sh.Hunt();
 Console.WriteLine(sh.Sleep());
 
-Console.WriteLine("\n");
+Console.WriteLine("\nPresentation of a crazy dog:\n");
 JackRussel jr = new JackRussel("dog", 2, 5, "dry food", "home, sometimes in the forest", "Jack Russel", "small", true);
 jr.DisplayInfo();
 jr.MakeSound();
